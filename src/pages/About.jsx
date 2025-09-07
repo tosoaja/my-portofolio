f3e9717022eacaa48e66a42c9476aacac39323a7
tosoaja <188399089+tosoaja@users.noreply.github.com>
2025-09-07 15:25:52 +0700

    Create About.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-3xl font-bold text-red-500">About Me</h1>
+      <p className="mt-4 text-gray-300">A short description about yourself.</p>
+    </div>
+  );
+}
