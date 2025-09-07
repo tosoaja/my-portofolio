5717281f0be00cd53ceabcb795e2a2e0c05b8078
tosoaja <188399089+tosoaja@users.noreply.github.com>
2025-09-07 15:26:26 +0700

    Create Projects.jsx

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,8 @@
+export default function Projects() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-3xl font-bold text-red-500">My Projects</h1>
+      <p className="mt-4 text-gray-300">Showcase your awesome work here.</p>
+    </div>
+  );
+}
