83c5a4f2e8d8482aad415c930a2de52acc7bda9c
tosoaja <188399089+tosoaja@users.noreply.github.com>
2025-09-07 15:26:45 +0700

    Create Contact.jsx

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-3xl font-bold text-red-500">Contact Me</h1>
+      <p className="mt-4 text-gray-300">Provide your contact details or a form.</p>
+    </div>
+  );
+}
